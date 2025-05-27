d0ea27e49c9616c90b8f3aaa4f82e2fdd17ce654
Diona Ymeraga <ymeragadiona@gmail.com>
2025-05-27 15:12:08 +0200

    Pokemon Interface and navbar

--- pokedex-ui-workshop/src/app/page.tsx
+++ pokedex-ui-workshop/src/app/page.tsx
@@ -1,20 +1,28 @@
-import PokemonCardComp from "@/components/pokemonCardComp";
+import PokemonsComp from "@/components/pokemonsComp";
 import PokeNavBar from "@/components/pokeNavBarComp";
 
 
 export default function Home() {
- const testData = {
-   pokemonNumber: 1,
-   pokemonName:"poke1",
-   pokemonType:["Water"],
-   mainImage:"https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png"
- };
+ const testData = [
+   {
+     pokemonNumber: 1,
+     pokemonName:"poke1",
+     pokemonType:["Water"],
+     mainImage: "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png"
+   },
+   {
+     pokemonNumber: 2,
+     pokemonName:"poke2",
+     pokemonType:["Fire"],     
+     mainImage: "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/002.png"
+   }
+ ];
 
 
  return (
    <>
      <PokeNavBar></PokeNavBar>
-     <PokemonCardComp pokemon={testData}></PokemonCardComp>
+     <PokemonsComp pokemons={testData}></PokemonsComp>
    </>
  );
 }
---
+++ pokedex-ui-workshop/src/model/pokemon.ts
@@ -0,0 +1,15 @@
+
+
+export interface Pokemon {
+  pokemonNumber: number;
+  attack: number;
+  defense: number;
+  devolution: string;
+  evolution: string;
+  evolutionFamily: string[];
+  healthPoints: number;
+  mainImage: string;
+  pokemonName: string;
+  pokemonType: string[];
+  speed: number;
+}
