fab0cc08a66a232df7fb6bed68d36d206d0ee5fa
Diona Ymeraga <ymeragadiona@gmail.com>
2025-05-27 14:34:00 +0200

    Added the Pokemon Tiling Component

--- pokedex-ui-workshop/src/app/page.tsx
+++ pokedex-ui-workshop/src/app/page.tsx
@@ -1,5 +1,20 @@
+import PokemonCardComp from "@/components/pokemonCardComp";
+import PokeNavBar from "@/components/pokeNavBarComp";
+
+
 export default function Home() {
+ const testData = {
+   pokemonNumber: 1,
+   pokemonName:"poke1",
+   pokemonType:["Water"],
+   mainImage:"https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png"
+ };
+
+
  return (
-  <>Hello World!</>
+   <>
+     <PokeNavBar></PokeNavBar>
+     <PokemonCardComp pokemon={testData}></PokemonCardComp>
+   </>
  );
 }
